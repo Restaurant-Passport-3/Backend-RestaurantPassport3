e862fcfbdfcbcd8ea1ff6254ce9bee10dd4c42c8
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-24 00:47:10 -0600

    adjusted knexfile

--- knexfile.js
+++ knexfile.js
@@ -34,11 +34,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    },
+
     migrations: {
       directory: "./database/migrations"
     },
