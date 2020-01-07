ca956a0bdc6035f03f565108ff55df4c90c722e2
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-01-07 16:37:42 -0600

    bug fixes

--- api/server.js
+++ api/server.js
@@ -5,17 +5,15 @@ const bodyParser = require("body-parser");
 
 const server = express();
 
-server.use(helmet());
-server.use(cors());
-// server.use(express.json());
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
-
 const authenticate = require("../routes/auth/authenticate-middleware.js");
 const authRouter = require("../routes/auth/auth-router.js");
 const usersRouter = require("../routes/users/users-router.js");
 const restaurantsRouter = require("../routes/restaurants/restaurants-router.js");
 const exploreRouter = require("../routes/restaurants/explore-router.js");
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", authenticate, usersRouter);
