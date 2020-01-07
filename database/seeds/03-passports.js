437b1366d952069ae2a89a49720934c5cce625d3
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-01-07 10:46:09 -0600

    Fixes for date on passport table

--- database/seeds/03-passports.js
+++ database/seeds/03-passports.js
@@ -1,8 +1,20 @@
 exports.seed = function(knex) {
   return knex("passports").insert([
-    { user_id: 1, restaurant_id: "QhzJXO6E_oLAx1Wz1Z_T2g" },
-    { user_id: 1, restaurant_id: "lPtvU9WezDkRzEvJge4sFw" },
-    { user_id: 2, restaurant_id: "lPtvU9WezDkRzEvJge4sFw" },
-    { user_id: 2, restaurant_id: "EMIHhPOUxZpnnXpwjOot6w" }
+    {
+      user_id: 1,
+      restaurant_id: "QhzJXO6E_oLAx1Wz1Z_T2g",
+      date: 1212331231231
+    },
+    {
+      user_id: 1,
+      restaurant_id: "lPtvU9WezDkRzEvJge4sFw",
+      date: 1212331231232
+    },
+    {
+      user_id: 2,
+      restaurant_id: "lPtvU9WezDkRzEvJge4sFw",
+      date: 1212331231233
+    },
+    { user_id: 2, restaurant_id: "EMIHhPOUxZpnnXpwjOot6w", date: 1212331231234 }
   ]);
 };
