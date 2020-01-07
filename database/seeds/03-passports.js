ee4eb0d46dda8b01a677b31d08593bbc9c5fcb46
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-01-07 10:56:58 -0600

    updated passport seeds

--- database/seeds/03-passports.js
+++ database/seeds/03-passports.js
@@ -3,22 +3,22 @@ exports.seed = function(knex) {
     {
       user_id: 1,
       restaurant_id: "QhzJXO6E_oLAx1Wz1Z_T2g",
-      date: date.now()
+      date: Date.now()
     },
     {
       user_id: 1,
       restaurant_id: "lPtvU9WezDkRzEvJge4sFw",
-      date: date.now()
+      date: Date.now()
     },
     {
       user_id: 2,
       restaurant_id: "lPtvU9WezDkRzEvJge4sFw",
-      date: date.now()
+      date: Date.now()
     },
     {
       user_id: 2,
       restaurant_id: "EMIHhPOUxZpnnXpwjOot6w",
-      date: date.now()
+      date: Date.now()
     }
   ]);
 };
